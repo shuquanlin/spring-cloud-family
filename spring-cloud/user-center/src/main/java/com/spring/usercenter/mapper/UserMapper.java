147592ad74440827755a4e1a85afefddff20cb12
shuquanlin <shuquanlin@newbeescm.com>
2018-08-30 17:34:01 +0800

    first

---
+++ spring-cloud/user-center/src/main/java/com/spring/usercenter/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.spring.usercenter.mapper;
+
+import com.spring.usercenter.domain.User;
+
+import java.util.List;
+
+/**
+ * @Author sql
+ * @Date 16:472018\7\9 0009
+ */
+public interface UserMapper {
+
+    List<User> queryUserInfoList();
+
+
+}
