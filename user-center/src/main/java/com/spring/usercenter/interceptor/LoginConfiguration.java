128c37b3b243d616f6430c3e15dbd41bae082b64
shuquanlin <shuquanlin@newbeescm.com>
2018-08-30 17:50:53 +0800

    测试提交

--- user-center/src/main/java/com/spring/usercenter/interceptor/LoginConfiguration.java
+++ user-center/src/main/java/com/spring/usercenter/interceptor/LoginConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
